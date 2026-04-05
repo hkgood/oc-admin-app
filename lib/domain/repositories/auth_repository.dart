149868c9c93e2e712162013104581e72228041d8
Rocky <72559939+hkgood@users.noreply.github.com>
2026-04-05 19:48:03 +0800

    Add ./lib/domain/repositories/auth_repository.dart

---
+++ lib/domain/repositories/auth_repository.dart
@@ -0,0 +1,8 @@
+import '../entities/user.dart';
+
+abstract class AuthRepository {
+  Future<User> login(String email, String password);
+  Future<User?> getCurrentUser();
+  Future<void> logout();
+  Future<bool> isLoggedIn();
+}
